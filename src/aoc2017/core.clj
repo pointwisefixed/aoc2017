666a107fe6c0892bad1d074be37f5a2c49e9a7c8
Gary Rosales <Gary.Rosales@walmart.com>
2017-12-04 04:53:59 -0500

    day4

--- src/aoc2017/core.clj
+++ src/aoc2017/core.clj
@@ -137,7 +137,25 @@
                  (:y next-coord)
                  (sum-adjacent-vals grid val next-coord)
                  next-move))))))
+(defn is-valid [line]
+  (empty? (filter (fn [[x freq]] (> freq 1)) (frequencies line))))
 
+(defn is-valid-with-anagram [line]
+  (let [sorted-line (map sort line)]
+    (empty? (filter (fn [[x freq]] (> freq 1)) (frequencies sorted-line)))))
+
+(defn day4-1 [lines]
+  (count (filter is-valid lines)))
+
+(defn day4-2 [lines]
+  (count (filter is-valid-with-anagram lines)))
+
+(defn day4 []
+  (let [lines-text (string/split-lines (slurp (io/resource "input/day4")))
+        lines (map #(string/split % #"\s") lines-text)]
+    (println (str "# of lines " (count lines)))
+    (println (day4-1 lines))
+    (println (day4-2 lines))))
 
 (defn day3-2 [input]
   (let [val (create-spiral-grid-2 input)]
@@ -155,5 +173,4 @@
 (defn -main
   "AOC YAY"
   [& args]
-  (day3-1 361527)
-  (day3-2 361527))
+  (day4))
